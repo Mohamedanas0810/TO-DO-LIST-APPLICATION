70c0f9d8c7d07a108c1f7930f2bc0b5839aba2d6
mohamedanas08 <mohamedanas0810@gmail.com>
2023-12-02 18:01:08 +0530

    changes in js file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var button = document.querySelector(".add");
 var Listtable = document.querySelector(".list-container");
 var alertmsg = document.querySelector(".alert");
+var inputvalue = document.querySelector(".inputfield");
+//While Clicking the Button
 button.addEventListener("click", function () {
     var inputvalue = document.querySelector(".inputfield");
     if (inputvalue.value === "") {
@@ -20,6 +22,27 @@ button.addEventListener("click", function () {
     }
 }
 )
+//While Click Enter Button
+inputvalue.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.keyCode === 13) {
+        if (inputvalue.value === "") {
+            alert("Enter a value");
+        }
+        else {
+            //Add Row by creating element
+            // let newrow = document.createElement("li");
+            // newrow.innerHTML = inputvalue.value;
+            // Listtable.appendChild(newrow);
+            //
+            //Add row by adding inner html
+            //\u00d7 - X symbol
+            Listtable.innerHTML = Listtable.innerHTML + "<li>" + inputvalue.value + "<button>\u00d7</button></li>";
+            inputvalue.value = "";
+            SaveData();
+        }
+    }
+})
+//For Table
 Listtable.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
@@ -33,10 +56,10 @@ Listtable.addEventListener("click", function (e) {
     // false - bubbling phase ( starts from target element to root elements)
     // true - capturing phase (starts from ancestor tags to target elements)
 }, false)
-function SaveData(){
+function SaveData() {
     localStorage.setItem("data", Listtable.innerHTML);
 }
-function GetData(){
+function GetData() {
     Listtable.innerHTML = localStorage.getItem("data");
 }
 GetData();
